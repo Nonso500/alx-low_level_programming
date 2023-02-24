d144303ba3b754e43af049c96248c9b9f0a0ba63
chinonso <christianasomugha5@gmail.com>
2023-02-24 13:09:30 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,19 @@
  */
 int main(void)
 {
-	int a;
+	int i;
 
-	for (a = 1; a <= 100; a++)
+	for (i = 1; a <= 100; i++)
 	{
-		if (a % 15 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-		else if (a % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (a % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%a", a);
-		if (a < 100)
+		if (i < 100)
 			printf(" ");
 	}
 	printf("\n");
