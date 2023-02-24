a6aa19a4191212cfd834f66a44a36b077dc8245b
Nonso500 <122862466+Nonso500@users.noreply.github.com>
2023-02-24 16:49:20 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the alphabet in lowercase, and then in uppercase,
- * followed by a new line
+ * followed by a new line by nonso
  * Return: Always 0 (Success)
  */
 int main(void)
