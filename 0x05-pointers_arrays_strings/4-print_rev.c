b259160247b3cbfb8264b2c7f210618bd7f1c712
chinonso <christianasomugha5@gmail.com>
2023-02-28 15:24:43 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_rev - print reverse
+* @s: string
+* return: 0
+*/
+void print_rev(char *s)
+{
+int count = 0;
+int x;
+while (*s != '\0')
+{
+count++;
+s++;
+}
+for (x = count; x > 0; x--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
