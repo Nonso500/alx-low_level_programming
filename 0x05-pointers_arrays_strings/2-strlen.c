a083ac516239e71e1eca779960ae5181ee7a9665
chinonso <christianasomugha5@gmail.com>
2023-02-28 14:57:48 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen - return the length of a string
+* @s: string
+* Return: length
+*/
+int _strlen(char *s)
+{
+int count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
