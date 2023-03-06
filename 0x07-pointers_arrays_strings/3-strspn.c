1d8b42498ffe4c4f2fd76b058a2bd8807c7468c8
chinonso <christianasomugha5@gmail.com>
2023-03-06 09:20:59 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ *
+ * @s: the initial segment of
+ *
+ * @accept: which consist only of bytes from
+ *
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int z = 0;
+
+	for (i = 0; i < s[i] != '\0'; i++)
+	{
+		for (j = 0; j < accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				z++;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			return (z);
+		}
+	}
+	return (z);
+}
