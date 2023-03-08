bb3d2da267763f9cbe8090cd0d62ea81e858c9dc
chinonso <christianasomugha5@gmail.com>
2023-03-08 09:36:18 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * factorial - factorial of int
+ * @n:int
+ * Return:int
+ */
+int factorial(int n)
+{
+
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+	int main(void)
+	{
+		int n;
+
+		 n = 1;
+		_putchar(n);
+	}
+}
