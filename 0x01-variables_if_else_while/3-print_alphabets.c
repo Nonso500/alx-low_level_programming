5cc9f89c241e3c314f8f20688ea7f6e98cf6043a
nonso500 <christianasomugha5@gmail.com>
2023-05-21 16:59:45 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,16 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c = c + 1)
+	{
 		putchar(c);
-	putchar('\n');
+	}
 
-	for (c = 'A'; c <= 'Z'; c++)
+	for (c = 'A'; c <= 'Z'; c = c + 1)
+	{
 		putchar(c);
+	}
+
+		putchar('\n');
 	return (0);
+
 }
