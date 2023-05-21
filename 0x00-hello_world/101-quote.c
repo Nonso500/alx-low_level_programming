fbdc0cd653d2195f966c5082e7a3d20a360e6713
nonso500 <christianasomugha5@gmail.com>
2023-05-21 16:26:40 -0700

    2-print_alphabet.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <wchar.h>
+#include <stdio.h>
+/**
+ * fputws- Print a string of text
+ *
+ * Return: 1
+ */
+int fputws(const wchar_t *ws, FILE *stream);
+
+{
+	fputws("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n");
+		return (0);
+}
