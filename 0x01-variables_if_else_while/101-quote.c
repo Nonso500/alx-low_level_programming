001f12a749fb235c3f73655121bc872affb4d9fe
nonso500 <christianasomugha5@gmail.com>
2023-05-25 16:09:44 -0700

    1-main.h

---
+++ 0x01-variables_if_else_while/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- a C program that prints exactly and
+ * that piece of art is useful" - Dora Korpar, 2015-10-19,
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+		return (1);
+}
